12a0ae1cc201304a0ac8cf754662da98c613d857
Abdullah <abdullahtech0119@gmail.com>
2022-05-31 12:36:17 +0530

    insertion done in crud-node folder

---
+++ node-crud/index.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+var mongoose = require('mongoose');
+var User = require('./models/index.js');
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+mongoose.connect("mongodb://localhost:27017/Crud", {useUnifiedTopology:true, useNewUrlParser: true});
+var connection = mongoose.connection;
+
+connection.once('open', function() {
+	console.log("Connection successfylly done ...");
+});
+
+app.set('view engine' , 'ejs');
+
+app.get("/", function(req, res) {
+	res.render("insert");
+});
+
+app.post("/insert", function(req, res) {
+	var user = new User({
+		name:req.body.name,
+		email:req.body.email,
+		password:req.body.password
+	})
+	user.save(()=>{
+		res.send("Data inserted");
+	})
+})
+
+app.listen(8000, function() {
+	console.log("Server started on 8000");
+});
